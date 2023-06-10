cfeff884dfa997fe8b7d3c23b6485d99947a82b2
M-Talhaouy <tal7aouy.dev@gmail.com>
2023-06-10 14:03:51 +0100

    add traits

---
+++ src/Entity/Traits/Slug.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait Slug
+{
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
